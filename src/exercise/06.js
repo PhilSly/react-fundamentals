669cdee0fa611aa0f95eba661f657271dc752def
Phil Sylvester <phil.sylvester@metcash.com>
2020-10-05 17:03:29 +1100

    Complete

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -18,13 +18,47 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input by specifying an `id` on
   // the input and a matching value as an `htmlFor` prop on the label.
+  const usernameRef = React.useRef('');
+  const [username, setUsername] = React.useState('');
+  const [errorMessage, setErrorMessage] = React.useState('');
+  function handleSubmit(event){ 
+    event.preventDefault();
+    // console.log(event);
+    // onSubmitUsername(document.getElementById('username-input').value);
+    onSubmitUsername(usernameRef.current.value);
+
+  }
+  function handleChange(event) {
+    const inputValue = usernameRef.current.value;
+    const inputValueLength = inputValue.length;
+    const lastTyped = inputValueLength > -1 ? inputValue.slice(inputValueLength-1) : '';
+
+    console.log(inputValue.charAt(inputValue.length), lastTyped)
+    if(lastTyped !== '' && lastTyped === lastTyped.toUpperCase()){
+      setErrorMessage(`Can't have capitals - you typed ${lastTyped}`);
+      // setUsername(inputValue);
+    }
+    else{
+      setErrorMessage('');
+      setUsername(inputValue);
+    }
+  }
+
+  
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username-input">Username:</label>
+        <input id="username-input" value={username} ref={usernameRef} type="text" onChange={handleChange} />
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={!username || errorMessage ? true : false} type="submit">Submit</button>
+      {/* {!username && errorMessage ? 'true' : 'false'}  */}
+      {username && 
+        <p>Your username will be {username}</p>
+      }
+      {errorMessage && 
+        <p>{errorMessage}</p>
+      }
     </form>
   )
 }
--- src/exercise/07.js
+++ src/exercise/07.js
@@ -18,6 +18,7 @@ function App() {
   }
 
   function removeItem(item) {
+    console.log(item);
     setItems(items.filter(i => i !== item))
   }
 
@@ -37,7 +38,7 @@ function App() {
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.value}-input`}>{item.value}</label>{' '}
             <input id={`${item.value}-input`} defaultValue={item.value} />
